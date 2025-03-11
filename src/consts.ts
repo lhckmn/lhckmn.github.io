7ff95d0bfde4ff094cb452006208f6aa1190c2fc
lhckmn <leander.hackmann@gmail.com>
2025-03-11 06:48:49 +0000

    Adapted header and footer

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Embedded Universe';
+export const SITE_DESCRIPTION = 'Embedded Systems, HAM radio & more by DK1LH';
